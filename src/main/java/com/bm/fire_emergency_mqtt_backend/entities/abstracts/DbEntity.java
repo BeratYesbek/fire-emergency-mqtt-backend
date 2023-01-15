8750e68160a5fb63244d71cf3d412e3343cd7f35
Feanor <beratyesbek@gmail.com>
2023-01-15 11:20:00 +0300

    Unused imports removed on DbEntity

--- src/main/java/com/bm/fire_emergency_mqtt_backend/entities/abstracts/DbEntity.java
+++ src/main/java/com/bm/fire_emergency_mqtt_backend/entities/abstracts/DbEntity.java
@@ -1,23 +1,22 @@
 package com.bm.fire_emergency_mqtt_backend.entities.abstracts;
 
 import lombok.*;
-import org.hibernate.Hibernate;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Objects;
-@Data
+
+@Getter
+@Setter
 @MappedSuperclass
-@ToString(of = {"id","createdAt"})
+@ToString(of = {"id", "createdAt"})
 public abstract class DbEntity implements Serializable {
 
     @Id
     @Access(AccessType.PROPERTY)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected int id;
-
 
     @Column(name = "created_at", nullable = false)
     @CreationTimestamp
