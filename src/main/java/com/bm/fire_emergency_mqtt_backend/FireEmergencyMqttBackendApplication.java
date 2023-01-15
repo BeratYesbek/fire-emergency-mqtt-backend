cd53fab19d4c6914ab0aeb210a71005dc59671b3
Feanor <beratyesbek@gmail.com>
2023-01-15 10:22:41 +0300

    initial commit

---
+++ src/main/java/com/bm/fire_emergency_mqtt_backend/FireEmergencyMqttBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bm.fire_emergency_mqtt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireEmergencyMqttBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireEmergencyMqttBackendApplication.class, args);
+    }
+
+}
