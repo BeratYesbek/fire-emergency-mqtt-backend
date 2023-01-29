76c75edbd9d26216fb79551a2c8a8c763e9d2364
Feanor <beratyesbek@gmail.com>
2023-01-29 12:00:21 +0300

    BaseServiceRepository implemented

--- src/main/java/com/bm/fire_emergency_mqtt_backend/services/common/BaseServiceRepository.java
+++ src/main/java/com/bm/fire_emergency_mqtt_backend/services/common/BaseServiceRepository.java
@@ -1,4 +1,9 @@
 package com.bm.fire_emergency_mqtt_backend.services.common;
 
 public interface BaseServiceRepository<T> {
+
+    T create(T entity);
+    T update(T entity, int id);
+    void delete(int id);
+
 }
